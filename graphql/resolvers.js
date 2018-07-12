f9a90e7dba57ab30108806518375c11b9bb9fcae
황진수-PC <cjswp122@gmail.com>
2018-07-12 13:28:45 +0900

    people create query with arguments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,30 @@
-const Jeckson = {
-  name: 'jeckson',
-  age: 31,
-  gender: 'male',
-}
+const People = [
+  {
+    id: 0,
+    name: 'jeckson',
+    age: 31,
+    gender: 'male',
+  },
+  {
+    id: 1,
+    name: 'jeckson1',
+    age: 11,
+    gender: 'male2',
+  },
+  {
+    id: 2,
+    name: 'jeckson2',
+    age: 21,
+    gender: 'male3',
+  },
+]
+
+const getById = id => People.filter(person => person.id === id)[0];
 
 const resolvers = {
   Query: {
-    person: () => Jeckson,
+    people: () => People,
+    person: (_, { id }) => getById(id),
   }
 }
 
